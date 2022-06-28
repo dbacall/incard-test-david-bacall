b3d8c1ba816f9d818eb6b7a7808492cfde69ffe5
dbacall <dbacall@hotmail.co.uk>
2022-06-28 13:15:33 +0100

    add styled-components and theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import Login from './pages/Login';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import { ThemeProvider } from 'styled-components';
+
+const theme = {
+  colors: {
+    text: 'white'
+  },
+  fonts: {
+    bold: 'Poppins-SemiBold'
+  }
+}
 
 function App() {
   return (
-    <div>
-      incard
-      <Login />
-    </div>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <div>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </div>
+      </Router>
+    </ThemeProvider>
   );
 }
 
