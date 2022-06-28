eb92c9339479fef3f67d741b64361f6df2c8cacc
dbacall <dbacall@hotmail.co.uk>
2022-06-28 13:15:45 +0100

    add first login page content

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,13 +1,41 @@
 import React, { FC } from 'react';
+import { ReactComponent as IncardLogo } from '../../assets/logo-incard.svg'
+import styled from 'styled-components';
 
 interface LoginProps {
-  
+
 }
- 
+
+const LoginContainer = styled.div`
+  padding: 72px 24px;
+`
+
+const Title = styled.h1`
+  color: ${props => props.theme.colors.text};
+  font-size: 32px;
+  font-family: ${props => props.theme.fonts.bold};
+  padding: 0;
+  margin-top: 56px
+`
+const LoginInstruction = styled.p`
+  color: ${props => props.theme.colors.text};
+  font-size: 16px
+`
+
+
 const Login: FC<LoginProps> = () => {
-  return ( 
-    <h1>Login</h1>
+  return (
+    <LoginContainer>
+      <IncardLogo />
+      <Title>
+        Hello!
+      </Title>
+      <LoginInstruction>
+        Log in to your incard account.
+      </LoginInstruction>
+
+    </LoginContainer>
   );
 }
- 
+
 export default Login;
